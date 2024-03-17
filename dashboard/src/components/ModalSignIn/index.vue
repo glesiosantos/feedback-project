1fc32aa112d4bdcacad82fe92ddd582bd5c4c306
Glêsio Santos <glesioss@gmail.com>
2024-03-17 11:34:20 -0300

    ajust modal

--- dashboard/src/components/ModalSignIn/index.vue
+++ dashboard/src/components/ModalSignIn/index.vue
@@ -1,3 +1,26 @@
+<script>
+import { reactive } from 'vue'
+export default {
+  setup() {
+    const state = reactive({
+      hasErrors: false,
+      isLoading: false,
+      email: {
+        value: '',
+        errorMessage: ''
+      },
+      password: {
+        value: '',
+        errorMessage: ''
+      }
+    })
+
+    return { state }
+  }
+}
+</script>
 <template>
-  <h1>Sign In Modal</h1>
+  <div class="flex justify-between">
+    <h1 class="text-4xl font-black">Entre na sua conta</h1>
+  </div>
 </template>
