a7c7c102322291dbb7faca40eb51a043a3453063
Glêsio Santos <glesioss@gmail.com>
2024-03-17 13:25:36 -0300

    build signin page with axios

--- dashboard/src/main.js
+++ dashboard/src/main.js
@@ -2,6 +2,9 @@ import 'animate.css'
 import './assets/main.css'
 import './assets/fonts.css'
 
+import 'vue-toastification/dist/index.css'
+import Toast, { POSITION } from 'vue-toastification'
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -9,5 +12,6 @@ import router from './router'
 const app = createApp(App)
 
 app.use(router)
+app.use(Toast, { position: POSITION.BOTTOM_RIGHT })
 
 app.mount('#app')
