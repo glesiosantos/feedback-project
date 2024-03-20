f19003bfb5e8e01bfd632546571c2d3afb2ccc30
Glêsio Santos <glesioss@gmail.com>
2024-03-20 20:30:10 -0300

    blocked user when local storage not exist token

--- dashboard/src/services/index.js
+++ dashboard/src/services/index.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
 
 import AuthService from './auth'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
 
 const API_ENVS = {
   local: 'http://localhost:3000'
@@ -8,6 +11,16 @@ const API_ENVS = {
 
 const httpClient = axios.create({
   baseURL: API_ENVS.local
+})
+
+httpClient.interceptors.request.use((config) => {
+  const token = window.localStorage.getItem('token')
+
+  if (token) {
+    config.headers.common.Authorization = `Bearer ${token}`
+  }
+
+  return config
 })
 
 // para os errors não cair catch
@@ -18,6 +31,10 @@ httpClient.interceptors.response.use(
 
     if (canThrowAnError) {
       throw new Error(error.message)
+    }
+
+    if (error.response.status === 401) {
+      router.push({ name: 'home' })
     }
 
     return error
